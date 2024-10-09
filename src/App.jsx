141dfc5a96c29806f5516276b4ca69c5fc39f814
fer <fer@fers-iMac.local>
2024-10-08 18:47:34 -0700

    koko

--- src/App.jsx
+++ src/App.jsx
@@ -48,6 +48,17 @@ import {
 function App() {
 
 
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => {
+      setShow(false)
+    }
+
+    const handleShow = () => setShow(true);
+
+
+
   const [items, setItems] = useState([]);
 
   const itemCollection = query(
@@ -106,185 +117,7 @@ function App() {
 
 
 
-  const[taskState, setTaskState]=useState({
-    comentarios:"",
-    direccionCliente:"",
-    correoCliente:"",
-    fechaMeta:"",
-    nombreCliente:"",
-    servicioRealizado:"",
-    tipoDeServicio:"",
-    telefonoCliente:""
-  })
 
-
-  const {
-    comentarios,
-    direccionCliente,
-    correoCliente,
-    fechaMeta,
-    nombreCliente,
-    servicioRealizado,
-    tipoDeServicio,
-    telefonoCliente
-  } = taskState
-
-
-  const handlerTaskState=({target})=>{
-      const {name, value} = target
-      setTaskState({...taskState, [name]:value})
-  }
-
-
-
-  const postCollection = collection(firestoreDB, 'RealControlCustomers');
-
-  const guardar =()=>{
-
-      if (comentarios.trim() === '' ||
-          direccionCliente.trim() === '' ||
-          telefonoCliente.trim() === '' ||
-          correoCliente.trim() === '' ||
-          fechaMeta.trim() === '' ||
-          nombreCliente.trim() === '' ||
-          servicioRealizado.trim() === '' ||
-          tipoDeServicio.trim() === '' ){
-              alert('Algun Campo esta Vacio')
-              return
-          }
-
-
-      if (confirm("Gaurdar Cliente")) {
-
-
-
-          taskState.dataArr = [{servicioRealizado,tipoDeServicio,lastTime:Date.now(),comentarios,fechaMeta}]
-
-          delete taskState.servicioRealizado
-          delete taskState.tipoDeServicio
-          delete taskState.comentarios
-          delete taskState.fechaMeta
-
-          addDoc(postCollection, taskState)
-          setTaskState({
-              comentarios:"",
-              direccionCliente:"",
-              telefonoCliente:"",
-              correoCliente:"",
-              fechaMeta:"",
-              nombreCliente:"",
-              servicioRealizado:"",
-              tipoDeServicio:""
-          })
-
-          setTimeout(()=>{
-            window.location.reload()
-          },600)
-          
-      }
-
-  }
-
-
-
-
-
-
-  const [updateMode, setUpdateMode]=useState(false)
-
-  const [newObj, setNewObj]=useState({
-    comentarios:"",
-    fechaMeta:"",
-    servicioRealizado:"",
-    tipoDeServicio:""
-  })
-
-
-
-  const handlerUpdateMode=({target})=>{
-      const {name, value} = target
-      setNewObj({...newObj, [name]:value})
-  }
-
-  const [saveObj, setSaveObj]=useState()
-  const [saveID, setSaveID]=useState()
-
- 
-
-
-  const updateById = async (id, obj) => {
-
-      setSaveObj(obj)
-      setSaveID(id)
-
-      if(updateMode === false){
-          setUpdateMode(true)
-          return
-      }
-
-      newObj.lastTime = Date.now()
-
-
-      saveObj.dataArr.push(newObj)
-
-
-      if (confirm("Añadir nueva info a Cliente")) {
-
-          delete saveObj.id
-
-          const aDoc = doc(firestoreDB, 'RealControlCustomers', saveID)
-
-          try {
-              await updateDoc(aDoc, saveObj);
-          } catch (error) {
-              console.error(error);
-          }
-
-          setUpdateMode(false)
-          setToggle(!toggle)
-
-      }
-
-  }
-
-
-
-    // const [clienteNameFinder, setClienteNameFinder]=useState()
-
-    // const handlerFinder =(e)=>{
-    //     if(e.target.value.length > 3){
-    //         setClienteNameFinder(e.target.value)
-    //     }
-    // }
-
-
-
-    // const [show, setShow] = useState(false);
-
-    // const handleClose = () => {
-    //   setUpdateMode(false)
-    //   setShow(false)
-    // }
-
-    // const handleShow = () => setShow(true);
-
-  let myEventsList =[{
-      start:dayjs('2024-10-4T12:00:00').toDate(),
-      end:dayjs('2024-10-4T12:00:00').toDate(),
-      title:'Primer evento',
-      data:{
-        d:'10'
-      }
-  },{
-      start:dayjs('2024-10-5T7:00:00').toDate(),
-      end:dayjs('2024-10-5T8:00:00').toDate(),
-      title:'Segundo evento',
-      data:{
-        d:'20'
-      }
-  }]
-
-console.log(myEventsList)
 
   const messages = {
     allDay: "Todo el día",
@@ -305,8 +138,11 @@ console.log(myEventsList)
 const components={
   event: e =>{
     // console.log(e)
-    return<div className='events'>
-        <span>{e.title}</span>
+    return<div className='events' onClick={()=>{
+                                                  handleShow()
+                                                  localStorage.customerName=e.title
+                                                }}>
+        <span>{e.title.slice(0,12)}</span>
     </div>
   }
 }
@@ -342,6 +178,26 @@ const handleSelected = (event) => {
                 components={components}
             />
         </div>
+
+
+
+
+
+
+
+
+        <Modal show={show} onHide={handleClose} centered>
+
+        <Modal.Header closeButton>
+          <Modal.Title>Cliente: </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+            {localStorage.customerName}
+        </Modal.Body>
+
+    
+      </Modal>
        
 
 
